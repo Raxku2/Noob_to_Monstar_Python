669cbe2c801bbc57fa3584d59a0ecf1fbf6893bf
Rakesh Kundu <159798591+rax-2@users.noreply.github.com>
2025-08-19 18:55:36 +0530

    Create pr.py

---
+++ Chapter_04/Problem/pr.py
@@ -0,0 +1,25 @@
+print(5+2, 5-2, 5*2, 5/2, 5%2, 5//2, 5**2)
+
+x = int(input())
+print("Even" if x%2==0 else "Odd")
+
+print(10>5, 10<5)
+
+print(True and False, True or False, not True)
+
+a = 5
+a += 3
+print(a)
+
+a,b = 7,10
+print("Greater:", a if a>b else b)
+
+print(5&3, 5|3, 5^3)
+
+print("py" in "python")
+
+x=[1,2]; y=[1,2]
+print(x==y, x is y)
+
+n=int(input())
+print(n**2)
