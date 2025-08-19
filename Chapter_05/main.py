0e9e5f70c94bf8e5e21b05bcc580eba23b299a0a
Rakesh Kundu <159798591+rax-2@users.noreply.github.com>
2025-08-19 18:57:20 +0530

    Create main.py

---
+++ Chapter_05/main.py
@@ -0,0 +1,10 @@
+s = " Python Programming "
+print(s.lower())
+print(s.upper())
+print(s.strip())
+print(s.replace("Python", "Java"))
+print(s.split())
+
+print("Count of 'o':", s.count("o"))
+print(s.startswith(" Python"))
+print(s.endswith("ing "))
