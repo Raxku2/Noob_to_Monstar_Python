da9dab7e90b9504309375afa1b75a5e73102edc8
Rakesh Kundu <159798591+rax-2@users.noreply.github.com>
2025-08-19 18:36:30 +0530

    Create pr.py

---
+++ Chapter_01/Chapter_02/Practice/pr.py
@@ -0,0 +1,44 @@
+# Solutions for Practice Set – Chapter 02
+
+# Q1
+city = "Kolkata"
+print(city)
+
+# Q2
+age = 21
+print(type(age))
+
+# Q3
+# num = "10" + 5   # Error: cannot add string and integer
+
+# Q4
+roll = 23
+marks = 88.5
+name = "Amit"
+print(roll, marks, name)
+
+# Q5
+# Valid: _marks, name_1
+
+# Q6
+a = 5
+b = 3.2
+c = "Python"
+print(a, type(a), b, type(b), c, type(c))
+
+# Q7
+flag = True
+print(flag)
+
+# Q8
+fruits = ["apple", "banana", "mango", "grape", "orange"]
+print(fruits, type(fruits))
+
+# Q9
+# list = ordered, mutable
+# tuple = ordered, immutable
+# set = unordered, unique values
+
+# Q10
+student = {"name": "Rahul", "age": 20, "marks": 85}
+print(student)
