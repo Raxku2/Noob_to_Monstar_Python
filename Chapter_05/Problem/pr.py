58d5d675eb8961b099d9ee30902b99fa8a4edb94
Rakesh Kundu <159798591+rax-2@users.noreply.github.com>
2025-08-19 18:57:56 +0530

    Create pr.py

---
+++ Chapter_05/Problem/pr.py
@@ -0,0 +1,21 @@
+s="Python"
+print(s[0], s[-1])
+
+s="Programming"
+print(s[3:7])
+
+print("hello".upper())
+
+print(" Hello ".strip())
+
+print("Hello World".replace("World","Python"))
+
+print("Banana".count("a"))
+
+print("apple,banana,mango".split(","))
+
+print("Hello World".startswith("Hello"))
+
+print("Learning".endswith("ing"))
+
+print("Hello"+" "+"Python")
