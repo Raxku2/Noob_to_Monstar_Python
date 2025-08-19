3c9747e98727bc9f185ed42fbde1b5ab3bae4f57
Rakesh Kundu <159798591+rax-2@users.noreply.github.com>
2025-08-19 18:54:35 +0530

    Create main.py

---
+++ Chapter_04/main.py
@@ -0,0 +1,18 @@
+print(10 + 5)
+print(10 // 3)
+print(2 ** 3)
+
+x = 5
+print(x > 3 and x < 10)
+
+a = 5
+a += 2
+print(a)
+
+nums = [1,2,3]
+print(2 in nums)
+
+x = [1,2]
+y = [1,2]
+print(x == y)
+print(x is y)
