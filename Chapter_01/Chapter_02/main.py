8aec4d5a68275075c3c8fd39fd6ca8546cefe6ec
Rakesh Kundu <159798591+rax-2@users.noreply.github.com>
2025-08-19 18:32:08 +0530

    Create main.py

---
+++ Chapter_01/Chapter_02/main.py
@@ -0,0 +1,21 @@
+# Variables & Data Types Example
+
+# Integer
+x = 10
+print("x:", x, "| Type:", type(x))
+
+# Float
+pi = 3.14
+print("pi:", pi, "| Type:", type(pi))
+
+# String
+name = "Rahul"
+print("name:", name, "| Type:", type(name))
+
+# Boolean
+flag = True
+print("flag:", flag, "| Type:", type(flag))
+
+# List
+nums = [1, 2, 3]
+print("nums:", nums, "| Type:", type(nums))
